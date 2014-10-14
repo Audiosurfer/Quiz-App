f8a3a896478410edbd98b366d243a538d5a809ea
Audiosurfer <gabolad@hotmail.com>
2014-10-14 11:48:29 -0500

    finished quiz app
    changed the way that it cycles through questions, so now instead of
    being statically loaded, it's done dynamically through javascript. also
    added songs & it now alerts you whether you're right or wrong and counts
    your score. will edit the way right/wrong is indicated later but alerts
    will have to do for now

--- app.js
+++ app.js
@@ -1,15 +1,66 @@
-$(document).ready(function() {
-    $('#startgame').on('click', function() {
-        $(this).parent().parent().addClass('hidden');
-        $(this).parent().parent().next().removeClass('hidden');
-    })
+$(document).ready(function () {
+    "use strict";
+    var qcount = 0,
+        rightCount = 0,
+        addOptions = function () {
+            $('audio').attr('src', questionList[qcount].song);
+            for (var i=0; i < questionList[i].options.length; i++) {
+                $('.quizopt').append("<img src="+ questionList[qcount].options[i]+" />");
+        }
+    };
     
-    $('img').on('dblclick', function() {
-        $(this).parent().parent().addClass('hidden');
-        $(this).parent().parent().next().removeClass('hidden');
-    })
+    var correctCheck = function() {
+        var choice = $('.selected').attr('src');
+        if (choice === questionList[qcount].correct) {
+            alert("Got it right!");
+            rightCount += 1;
+        }
+        else {
+            alert("Got it wrong");
+        }
+    }
     
-//    var questionCheck = function() {
+    var showResults = function() {
+        if (qcount > 4) {
+            $('#quizspot').addClass('hidden');
+            $('#results').removeClass('hidden');
+            $('#results h1').text('You scored ' + rightCount + '/5. Play again?');
+        }
+    }
+    
+    var restartGame = function() {
+        $('#results').addClass('hidden');
+        $('#quizspot').removeClass('hidden');
+        qcount = 0;
+        rightCount = 0;
+        addOptions();
+    }
+    
+    $('#howtoplay').on('click', function() {
+        $('#tutorial').toggleClass('hidden');
+    })
         
+    $('#startgame').on('click', function() {
+        $('#startplace').addClass('hidden');
+        $('#quizspot').removeClass('hidden');
+        addOptions();
+    })
+     
+    $('.quizopt').on('click', 'img', function() {
+        $(this).toggleClass('selected');
+    })
+     
+    $('#continue').on('click', function() {
+        correctCheck();
+        $('.quizopt').empty();
+        qcount +=1; 
+        showResults();
+        addOptions();
+        
+    })
+    
+    $('#restart').on('click', function() {
+        restartGame();
+    })
 });
 
--- questions.js
+++ questions.js
@@ -1,16 +1,17 @@
-function Question (opt1, opt2, opt3, opt4, correct) {
+function Question(song, opt1, opt2, opt3, opt4, correct) {
+    this.song = song;
     this.options = [opt1, opt2, opt3, opt4];
     this.correct = this.options[correct];
 }
 
-var q1 = new Question("Good Kid M.A.A.D City", "My Beautiful Dark Twisted Fantasy", "Madvillainy", "Aquemini", 3);
+var q1 = new Question("songs/m.A.A.d%20City.mp3", "album-covers/kendricklamar-goodkidmaadcity.jpg", "album-covers/kanyewest-mybeautifuldarktwistedfantasy.jpg", "album-covers/madvillain-madvillainy.png", "album-covers/outkast-aquemini.jpg", 0);
 
-var q2 = new Question("Sung Tongs", "Rejoicing In The Hands", "The Milk-Eyed Mender", "This Is It And I Am It", 0);
+var q2 = new Question("songs/Peach,%20Plum,%20Pear.mp3", "album-covers/animalcollective-sungtongs.jpg", "album-covers/devendrabanhart-rejoicinginthehands.jpg", "album-covers/joannanewsom-themilkeyedmender.jpg", "album-covers/marniestern-thisisit.jpg", 2);
 
-var q3 = new Question("Moon Pix", "Once I Was An Eagle", "The Worse Things Get", 0);
+var q3 = new Question("songs/Every%20Single%20Night.mp3", "album-covers/catpower-moonpix.jpg", "album-covers/fionaapple-theidlerwheel.jpg", "album-covers/lauramarling-onceiwasaneagle.jpg", "album-covers/nekocase-theworsethingsget.jpg", 1);
 
-var q4 = new Question("LP1", "Devotion", "The Electric Lady", "Cut 4 Me", 2);
+var q4 = new Question("songs/Oblivion.mp3", "album-covers/fkatwigsalbumcover.png", "album-covers/jessieware-devotion.jpg", "album-covers/janellemonae-theelectriclady.png", "album-covers/grimes-visions.png", 3);
 
-var q5 = new Question("Slanted And Enchanted", "Rid Of Me", "Surfer Rosa", "Emergency And I", 1);
+var q5 = new Question("songs/05%20Gigantic.mp3", "album-covers/pavement-slantedandenchanted.jpg", "album-covers/pjharvey-ridofme.jpg", "album-covers/pixies-surferrosa.jpg", "album-covers/thedismembermentplan-emergencyandi.jpg", 2);
     
-
+var questionList = [q1, q2, q3, q4, q5];
